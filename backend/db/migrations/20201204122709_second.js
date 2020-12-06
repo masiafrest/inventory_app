5a14dc0fe70635094201733392bfa3a28175ab94
masiaf <masiafrest@gmail.com>
2020-12-06 15:47:12 -0500

    second file migrations done

--- backend/db/migrations/20201204122709_second.js
+++ backend/db/migrations/20201204122709_second.js
@@ -1,5 +1,3 @@
-//initial added table lugar, item, item_inventario, empleado, rol, compañia, telefono
-//this second add table cliente, recibo, detalle_venta, linea_venta
 const { tableNames, tipo_recibo, tipo_pago } = require('../../src/constants/string');
 const {
     addDefaultColumns,
@@ -14,48 +12,122 @@ const {
  * @param {import('knex')} knex
  */
 exports.up = async function (knex) {
-    await knex.schema.createTable(tableNames.cliente, table => {
-        createTableIncrementsStringNotNullable(table, 'nombre');
-        references(table, tableNames.telefono);
-        addEmail(table);
-        addUrl(table, 'image_url');
-    });
-    await knex.schema.createTable(tableNames.recibo, table => {
-        createTableIncrementsStringNotNullable(table);
-        references(table, tableNames.cliente);
-        references(table, tableNames.empleado);
-        table.enum('tipo', (Object.values(tipo_recibo)))
-    })
-    await knex.schema.createTable(tableNames.detalle_venta, table => {
-        references(table, tableNames.recibo, true, '', true);
-        table.enum('tipo_pago', Object.values(tipo_pago));
-        table.float('subtotal').unsigned().notNullable();
-        table.float('total').unsigned().notNullable();
-        table.boolean('pagado').notNullable();
-    })
-    await knex.schema.createTable(tableNames.linea_venta, table => {
+    await knex.schema.createTable(tableNames.cliente_empleado, table => {
         table.increments().notNullable();
-        references(table, tableNames.recibo);
+        references(table, tableNames.empresa_cliente);
+        references(table, tableNames.empleado);
+        addDefaultColumns(table);
+    })
+    await knex.schema.createTable(tableNames.cotizacion, table => {
+        table.increments().notNullable();
+        references(table, tableNames.cliente_empleado);
+        table.float('total');
+        table.float('sub_total');
+        table.float('tax');
+    })
+    await knex.schema.createTable(tableNames.garantia, table => {
+        table.increments().notNullable();
+        references(table, tableNames.cliente_empleado);
+        table.boolean('resuelto');
+    })
+
+    await knex.schema.createTable(tableNames.nota_credito, table => {
+        table.increments().notNullable();
+        references(table, tableNames.cliente_empleado);
+        table.float('total');
+    })
+    await knex.schema.createTable(tableNames.devolucion, table => {
+        table.increments().notNullable();
+        references(table, tableNames.cliente_empleado);
+        table.float('total');
+    })
+    await knex.schema.createTable(tableNames.pago, table => {
+        table.increments().notNullable();
+        table.float('nequi').unsigned();
+        table.float('transferencia').unsigned();
+        table.float('efectivo').unsigned();
+        table.float('yappi').unsigned();
+        references(table, tableNames.nota_credito);
+        references(table, tableNames.nota_credito, true, tableNames.nota_credito + '_2');
+    })
+    await knex.schema.createTable(tableNames.venta, table => {
+        table.increments().notNullable();
+        references(table, tableNames.cliente_empleado);
+        table.float('total');
+        table.float('sub_total');
+        table.float('tax');
+        table.boolean('pagado');
+        table.boolean('entregado');
+        references(table, tableNames.pago);
+    })
+    await knex.schema.createTable(tableNames.linea_venta_cotizacion, table => {
+        table.increments().unsigned();
+        references(table, tableNames.venta, false);
+        references(table, tableNames.cotizacion, false);
+        table.integer('qty').unsigned();
+        table.float('total');
+        table.float('tax');
+    })
+    await knex.schema.createTable(tableNames.linea_garantia, table => {
+        table.increments().unsigned();
+        references(table, tableNames.garantia);
         references(table, tableNames.item);
-        table.string('qty').unsigned().notNullable();
-        table.float('total').unsigned().notNullable();
+        references(table, tableNames.venta, false);
+        table.integer('qty');
+        table.string('descripcion', 500);
+    })
+    await knex.schema.createTable(tableNames.linea_nota_credito, table => {
+        table.increments().unsigned();
+        references(table, tableNames.garantia);
+        references(table, tableNames.nota_credito);
+        references(table, tableNames.venta, false);
+        table.boolean('valido');
+        table.string('descripcion', 500);
+    })
+    await knex.schema.createTable(tableNames.linea_devolucion, table => {
+        table.increments().unsigned();
+        references(table, tableNames.garantia);
+        references(table, tableNames.item, false, 'item_salida');
+        references(table, tableNames.item, false, 'item_entrada');
+        references(table, tableNames.devolucion);
+        table.integer('qty');
+        table.string('descripcion', 500);
+        table.integer('total');
+    })
+    await knex.schema.createTable(tableNames.item_inventario_log, table => {
+        references(table, tableNames.item, true, '', true);
+        references(table, tableNames.proveedor, false)
+        references(table, tableNames.empleado);
+        table.enum('evento', ['transferencia', 'venta', 'garantia', 'devolucion', 'nota credito']).notNullable();
+        table.integer('ajuste').notNullable();
+    })
+    await knex.schema.createTable(tableNames.precio_log, table => {
+        table.increments().notNullable();
+        references(table, tableNames.precio);
+        references(table, tableNames.empleado);
+        references(table, tableNames.proveedor, false)
+        table.float('precio_viejo');
+        table.float('costo_viejo');
+        table.float('precio_min_viejo');
     })
 };
 
 exports.down = async function (knex) {
-    await knex.schema.dropTableIfExists(tableNames.linea_venta);
-    await knex.schema.dropTableIfExists(tableNames.detalle_venta);
-    await knex.schema.dropTableIfExists(tableNames.recibo);
-    await knex.schema.dropTableIfExists(tableNames.cliente);
-    /*     await Promise.all(
-            [
-                tableNames.detalle_venta,
-                tableNames.recibo,
-                tableNames.cliente,
-                tableNames.linea_venta
-            ].map(tableName => {
-                console.log('dropping table ', tableName);
-                knex.schema.dropTableIfExists(tableName);
-            })
-        ) */
+    await Promise.all(
+        [
+            tableNames.precio_log,
+            tableNames.item_inventario_log,
+            tableNames.linea_devolucion,
+            tableNames.linea_nota_credito,
+            tableNames.linea_garantia,
+            tableNames.linea_venta_cotizacion,
+            tableNames.venta,
+            tableNames.pago,
+            tableNames.devolucion,
+            tableNames.nota_credito,
+            tableNames.garantia,
+            tableNames.cotizacion,
+            tableNames.cliente_empleado,
+        ].map(tableName => knex.schema.dropTableIfExists(tableName))
+    );
 };
--- backend/src/constants/string.js
+++ backend/src/constants/string.js
@@ -12,17 +12,17 @@ const tableNames = {
     //
     cliente_empleado: 'cliente_empleado',//dpnd on empresa_cliente.id, empleado.id
     cotizacion: 'cotizacion',//dpnd on cliente_empleado.id
-    venta: 'venta',//dpnd on cliente_empleado.id 
     garantia: 'garantia',//dpnd on cliente_empleado.id
     nota_credito: 'nota_credito',//dpnd on cliente_empleado.id
     devolucion: 'devolucion',//dpnd on  cliente_empleado.id
+    pago: 'pago', //dpnd on nota_credito.id, cheque.id, 
+    venta: 'venta',//dpnd on cliente_empleado.id, pago.id 
     linea_venta_cotizacion: 'linea_venta_cotizacion',//dpnd on item.id, cotizacion.id || venta.id
     linea_garantia: 'linea_garantia',//dpnd on item.id, garantia.id, venta.id
     linea_nota_credito: 'linea_nota_credito',//dpnd on nota_credito.id, garantia.id, venta.id
     linea_devolucion: 'linea_devolucion',//dpnd on garantia.id, item.id as salida y entrada, devolution.id
     item_inventario_log: 'item_inventario_log',//dpnd on item.id, proveedor.id, empleado.id 
     precio_log: 'precio_log', //dpnd on precio.id, empleado.id,
-    pago: 'pago', //dpnd on nota_credito.id, cheque.id, 
 }
 const tipo_pago = {
     yappi: 'yappi',
