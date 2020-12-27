c982fd3ab6aa9511bf9ef95cfbf528a9d7230711
masiaf <masiafrest@gmail.com>
2020-12-27 15:44:51 -0500

    fix bug

--- backend/src/api/recibos/garantias/garantias.routes.js
+++ backend/src/api/recibos/garantias/garantias.routes.js
@@ -17,39 +17,39 @@ router.get("/", async (req, res, next) => {
 
 router.post("/", async (req, res, next) => {
   try {
-    //TODO hacer garantia
     await Garantia.transaction(async (trx) => {
       const { empresa_cliente_id } = req.body;
       await Promise.all(
         req.body.lineas.map(async (linea) => {
           const { inventario_id, venta_id } = linea;
-          //check is the client is in the venta recibo
-          const venta = await Venta.query().findById(venta_id);
-          if (venta.empresa_cliente_id !== empresa_cliente_id) {
-            res.status(406);
-            const error = new Error("el recibo no es de este cliente");
-            throw error;
-          }
-
-          //check is item exist on linea venta, (meaning is been sold to that client)
-          const lineasVentas = await Linea_venta.query().where({
-            venta_id,
-            inventario_id,
-          }); //this will return array
-          if (lineasVentas.length === 0) {
-            res.status(406);
-            const error = new Error(
-              "este recibo de venta no tiene este item listado"
-            );
-            throw error;
-          }
-          for (const line of lineasVentas) {
-            if (line.inventario_id !== inventario_id) {
+          if (venta_id) {
+            //check is the client is in the venta recibo
+            const venta = await Venta.query().findById(venta_id);
+            if (venta.empresa_cliente_id !== empresa_cliente_id) {
+              res.status(406);
+              const error = new Error("el recibo no es de este cliente");
+              throw error;
+            }
+            //check is item exist on linea venta, (meaning is been sold to that client)
+            const lineasVentas = await Linea_venta.query().where({
+              venta_id,
+              inventario_id,
+            }); //this will return array
+            if (lineasVentas.length === 0) {
               res.status(406);
               const error = new Error(
                 "este recibo de venta no tiene este item listado"
               );
               throw error;
+            }
+            for (const line of lineasVentas) {
+              if (line.inventario_id !== inventario_id) {
+                res.status(406);
+                const error = new Error(
+                  "este recibo de venta no tiene este item listado"
+                );
+                throw error;
+              }
             }
           }
         })
