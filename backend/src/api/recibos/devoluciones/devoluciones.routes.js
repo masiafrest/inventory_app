ff1eb330554da94c1a23d5a8b724bd2ed30f0993
masiaf <masiafrest@gmail.com>
2020-12-27 16:34:09 -0500

    add TODO

--- backend/src/api/recibos/devoluciones/devoluciones.routes.js
+++ backend/src/api/recibos/devoluciones/devoluciones.routes.js
@@ -14,6 +14,7 @@ router.get("/", async (req, res, next) => {
 });
 
 //TODO hacer post devolucion
+// devolucion: item devuelto por efectivo o otro item
 router.post("/", async (req, res, next) => {
   try {
     // TODO tal vez crear tabla de cliente_item_comprado, donde acumula los item comprado con columna cliente_id, inventario_id, qty, asi se resta de qty o suma, inv tal vez agregar columna en_reparacion
