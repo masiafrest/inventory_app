a48e67ffed19bb270c427186f4969672c65fe55f
masiaf <masiafrest@gmail.com>
2020-12-30 15:45:17 -0500

    add patch user

--- backend/src/api/usuarios/usuarios.routes.js
+++ backend/src/api/usuarios/usuarios.routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const Usuario = require("./usuarios.model");
 const router = express.Router();
 const { signUp } = require("../auth/auth.controllers");
 const { findByIdOrName } = require("../../lib/helpers");
+
+const Usuario = require("./usuarios.model");
 
 router.get("/", async (req, res, next) => {
   try {
@@ -20,10 +21,31 @@ router.get("/:x", async (req, res, next) => {
 // to post user, se usa auth desde rol jefa
 router.post("/addUser", signUp);
 
-// TODO: add patch
 router.patch("/:id", async (req, res, next) => {
   try {
     await Usuario.transaction(async (trx) => {
+      if ("password" in req.body) {
+        const bcrypt = require("bcrypt");
+        const yup = require("yup");
+        const yupSchema = require("yup")
+          .object()
+          .shape({
+            password: yup
+              .string()
+              .min(6)
+              .max(500)
+              .matches(
+                /[^A-Za-z0-9]/,
+                "contraseña debe tener un caracter especial"
+              )
+              .matches(/[A-Z]/, "contraseña debe tener una Mayuscula")
+              .matches(/[a-z]/, "contraseña debe tener minuscula")
+              .matches(/[0-9]/, "contraseña debe tener numeros"),
+          });
+        await yupSchema.validate(req.body, { abortEarly: false });
+        const hashedPassword = await bcrypt.hash(req.body.password, 12);
+        req.body.password = hashedPassword;
+      }
       const userPatch = await Usuario.query(trx).patchAndFetchById(
         req.params.id,
         req.body
