5d01b919e49edbcbdcebdb5688b7dafafebc1f85
masiaf <masiafrest@gmail.com>
2021-01-01 21:41:55 -0500

    add reducer

---
+++ client_react_ts/src/redux/reducers/uiReducers.ts
@@ -0,0 +1,23 @@
+import { stat } from "fs";
+import { SET_ERRORS, CLEAR_ERRORS, LOADING_UI } from "../types";
+
+const initialState = {
+  loading: false,
+  errors: null,
+};
+
+export default function (
+  state = initialState,
+  action: { type: any; payload: any }
+) {
+  switch (action.type) {
+    case SET_ERRORS:
+      return { ...state, loading: false, errors: action.payload };
+    case CLEAR_ERRORS:
+      return { ...state, loading: false, errors: null };
+    case LOADING_UI:
+      return { ...state, loading: true };
+    default:
+      return state;
+  }
+}
