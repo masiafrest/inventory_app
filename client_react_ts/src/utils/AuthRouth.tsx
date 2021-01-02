7f113eed37eda8be91503e41b6d6fd7bc2419fb9
masiaf <masiafrest@gmail.com>
2021-01-02 11:40:02 -0500

    fix ts pass component as props problem

--- client_react_ts/src/utils/AuthRouth.tsx
+++ client_react_ts/src/utils/AuthRouth.tsx
@@ -1,13 +1,22 @@
 import React from "react";
-import { Route, RouteProps } from "react-router-dom";
+import { Route, RouteComponentProps } from "react-router-dom";
 import { connect } from "react-redux";
 
-interface IAuthProps{
-  component: any;
+interface IAuthProps {
+  Component: React.FC<RouteComponentProps>; //maybe work on funcion component and not class component
   authenticated: boolean;
   rest: any;
 }
 
-const AuthRoute  = ({
-    component: Component, authenticated, ...rest 
-}: IAuthProps )
+const AuthRouth = ({ Component, authenticated, ...rest }: IAuthProps) => {
+  <Route
+    {...rest}
+    render={(props) =>
+      authenticated === true ? (
+        <Component {...props} />
+      ) : (
+        <Component {...props} />
+      )
+    }
+  />;
+};
