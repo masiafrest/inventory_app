6634541aade5e3a4552bef4bc9c40129e7ece55a
masiaf <masiafrest@gmail.com>
2021-01-10 17:42:57 -0500

    add toogle view button

--- client_react_ts/src/components/ShowData.tsx
+++ client_react_ts/src/components/ShowData.tsx
@@ -3,19 +3,38 @@ import React, { useState } from "react";
 import DataCard from "./DataCard";
 import DataAccordion from "./DataAccordion";
 
+import ToggleButton from "@material-ui/lab/ToggleButton";
+import ToggleButtonGroup from "@material-ui/lab/ToggleButtonGroup";
+import ViewHeadlineIcon from "@material-ui/icons/ViewHeadline";
+import ViewAgendaIcon from "@material-ui/icons/ViewAgenda";
+
 import { fakeReduxStore, itemData, image } from "../fakeDataToTest";
 
 export default function ShowData() {
   const [toggleView, setToggleView] = useState("accordion");
 
-  const onChangeHandler = () => {
-    console.log("heyyyyyyyyyyyyyyyyyyyyyyyyy", toggleView);
+  const onChangeHandler = (
+    event: React.MouseEvent<HTMLElement>,
+    newAlignment: string
+  ) => {
     setToggleView(toggleView === "accordion" ? "card" : "accordion");
   };
 
   return (
     <>
-      <button onClick={onChangeHandler}> toggle</button>
+      <ToggleButtonGroup
+        size="small"
+        value={toggleView}
+        exclusive
+        onChange={onChangeHandler}
+      >
+        <ToggleButton value="accordion">
+          <ViewHeadlineIcon fontSize="small" />
+        </ToggleButton>
+        <ToggleButton value="card">
+          <ViewAgendaIcon fontSize="small" />
+        </ToggleButton>
+      </ToggleButtonGroup>
       {toggleView === "accordion" ? (
         <DataAccordion data={itemData} />
       ) : (
