13110ef1749f4c43fa20324ccf69f42fb94ceba1
masiafrest <masiafrest@gmail.com>
2021-03-24 18:26:31 -0500

    add table to transferencia

---
+++ client_react_ts/src/pages/recibos/transferencia/components/orderTable/OrderTable.tsx
@@ -0,0 +1,88 @@
+import React, { ReactElement } from "react";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import IconButton from "@material-ui/core/IconButton";
+import Button from "@material-ui/core/Button";
+
+import AddBoxIcon from "@material-ui/icons/AddBox";
+import DeleteIcon from "@material-ui/icons/Delete";
+
+import { useDispatch } from "react-redux";
+import { deleteLinea } from "../../../../../redux/features/recibo/reciboSlice";
+//types
+import { ItemRow } from "./OrderTableContainer";
+
+const DelRow = ({ inv_id }) => {
+  const dispatch = useDispatch();
+  const handleClick = () => {
+    console.log(inv_id);
+    dispatch(deleteLinea(inv_id));
+    window.location.reload();
+  };
+  return (
+    <IconButton size='small' onClick={handleClick} >
+      <DeleteIcon />
+    </IconButton>
+  );
+};
+
+const ShowRows = ({ rows, ccyFormat }) => {
+  console.log(rows);
+  return rows.map((row) => (
+    <TableRow key={row.id}>
+      <TableCell  align="left">
+      <DelRow inv_id={row.id} />
+        {row.qty}</TableCell>
+      <TableCell align="left">{`${row.marca} ${row.modelo} ${row.color} ${row.descripcion} `}</TableCell>
+      <TableCell align="center">{row.precio}</TableCell>
+      <TableCell align="center">{ccyFormat(row.precio)}</TableCell>
+    </TableRow>
+  ));
+};
+
+function OrderTable({ items, ccyFormat, invoice, tax, onClickHandler }) {
+  const { Subtotal, Taxes, Total } = invoice;
+  return (
+    <TableContainer component={Paper}>
+      <Table style={{minWidth:425}} padding="default" size="small">
+        <TableHead>
+          <TableRow>
+            <TableCell align="center">Qty</TableCell>
+            <TableCell align="center">Descripcion</TableCell>
+            <TableCell align="center">Price</TableCell>
+            <TableCell align="center">Sum</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {<ShowRows rows={items} ccyFormat={ccyFormat} />}
+          <TableRow>
+            <IconButton size='small'  onClick={onClickHandler}>
+              <AddBoxIcon />
+            </IconButton>
+          </TableRow>
+          <TableRow>
+            <TableCell rowSpan={3} />
+            <TableCell colSpan={2}>Subtotal</TableCell>
+            <TableCell align="right">{ccyFormat(Subtotal)}</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell colSpan={1}>Tax</TableCell>
+            <TableCell align="right">{`${(tax * 100).toFixed(0)} %`}</TableCell>
+            <TableCell align="right">{ccyFormat(Taxes)}</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell colSpan={2}>Total</TableCell>
+            <TableCell align="right">{ccyFormat(Total)}</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
+
+export default OrderTable;
--- client_react_ts/src/pages/recibos/transferencia/index.tsx
+++ client_react_ts/src/pages/recibos/transferencia/index.tsx
@@ -1,7 +1,8 @@
 import TransferenciaTable from './components/TransferenciaTable'
+import Table from './components/orderTable/OrderTableContainer'
 
 export default function TransferenciaRecibo() {
   return (
-      <TransferenciaTable/>
+    <Table/>
   );
 }
