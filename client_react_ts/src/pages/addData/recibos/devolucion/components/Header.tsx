28704a2fbcdfc4f90cfd6853610f513503e51faf
masiaf <masiafrest@gmail.com>
2021-04-12 22:27:06 -0400

    add todo

--- client_react_ts/src/pages/addData/recibos/devolucion/components/Header.tsx
+++ client_react_ts/src/pages/addData/recibos/devolucion/components/Header.tsx
@@ -6,8 +6,9 @@ import { useDispatch } from "react-redux";
 import { addClienteId } from "../../../../../redux/features/recibo/reciboSlice";
 import { FormControlLabel, Checkbox } from "@material-ui/core";
 import { useStyle } from "../../../useStyle";
+import axios from "axios";
 
-export default function HeaderTable() {
+export default function Header() {
   const classes = useStyle();
   const dispatch = useDispatch();
   const [cliente] = useState([]);
@@ -15,6 +16,8 @@ export default function HeaderTable() {
   const onChangeHandler = (e) => {
     console.log(e.target.value);
     dispatch(addClienteId(e.target.value));
+    //enviar cliente id para buscar recibo venta_id
+
     // setCliente(e.target.value);
     console.log(cliente);
   };
