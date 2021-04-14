a93f5e393c4dd1d21863b8363555cedf21af4ea6
masiaf <masiafrest@gmail.com>
2021-04-13 21:08:39 -0400

    added select item to return

--- client_react_ts/src/pages/addData/recibos/devolucion/components/Header.tsx
+++ client_react_ts/src/pages/addData/recibos/devolucion/components/Header.tsx
@@ -2,28 +2,33 @@ import { useState } from "react";
 import SelectsOptions from "../../../components/SelectsOptions";
 import { Paper } from "@material-ui/core";
 
-import { FormControlLabel, Checkbox, Select, MenuItem } from "@material-ui/core";
+import {
+  FormControlLabel,
+  Checkbox,
+  Select,
+  MenuItem,
+} from "@material-ui/core";
 import { useStyle } from "../../../useStyle";
 import axios from "axios";
 
 export default function Header({ useStates }) {
   const classes = useStyle();
   const [clientId, setClientId] = useStates;
-  const [lineas, setLineas] = useState<{ ventas: any }>()
-  const [itemIdSel, setItemIdSel] = useState<number>()
-  const [itemIdSelected, setItemIdSelected] = useState()
+  const [lineas, setLineas] = useState<{ ventas: any }>();
+  const [itemIdSel, setItemIdSel] = useState<number>();
+  const [selectedItem, setSelectedItem] = useState<any>();
 
   const getLineasVentaYDevolucion = () => {
-    axios.post(`/devolucion/${clientId}`).then(res => {
-      setLineas(res.data)
-    })
-  }
+    axios.post(`/devolucion/${clientId}`).then((res) => {
+      setLineas(res.data);
+    });
+  };
 
-  // const menuItems = lineas.ventas.map(venta => (
-  //   <MenuItem key={venta.id} value={venta.item_id}>
-  //     {`${venta.item.marca} ${venta.item.modelo}`}
-  //   </MenuItem>
-  // ))
+  const menuItems = lineas.ventas.map((venta) => (
+    <MenuItem key={venta.id} value={venta.item_id}>
+      {`${venta.item.marca} ${venta.item.modelo}`}
+    </MenuItem>
+  ));
 
   return (
     <Paper>
@@ -35,17 +40,16 @@ export default function Header({ useStates }) {
         value={clientId}
       />
       <Select
-        onChange={(e) => setItemIdSelected(e.target.value)}
-        labelId={'lineas'}
-        id={'lineas'}
-        name={'lineas'}
+        onChange={(e) => setSelectedItem(e.target.value)}
+        labelId={"lineas"}
+        id={"lineas"}
+        name={"lineas"}
         value={1}
         fullWidth
       >
-        {/* {menuItems} */}
+        {menuItems}
       </Select>
-      {lineas ? null : 'lineas'}
-
+      {lineas ? null : "lineas"}
     </Paper>
   );
 }
